e81c502346fb599cf71155b30c1faec70a2b8d58
ChandimaHerath <chandimaherath@hotmail.com>
2021-10-21 00:17:17 +0530

    aauth-guard.ts added

---
+++ ts/auth-guard.ts
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+function canActivate(){
+    if (!localStorage.getItem('token')){
+        window.location.replace('http://localhost:1234/sign-in.html');
+    }else{
+        $(".overlay").remove();
+    }
+}
+
+canActivate();
+
